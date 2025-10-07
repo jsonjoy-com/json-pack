6a1b4b134c7aa70f379de3ca80fc4aedee76636b
streamich <streamich@gmail.com>
2025-10-07 15:29:39 +0200

    feat: 🎸 implement RM decoder

--- src/rm/RmRecordDecoder.ts
+++ src/rm/RmRecordDecoder.ts
@@ -1,7 +1,8 @@
-import {StreamingOctetReader} from '@jsonjoy.com/util/lib/buffers/StreamingOctetReader';
+import {StreamingReader} from '@jsonjoy.com/buffers/lib/StreamingReader';
+import {concatList} from '@jsonjoy.com/buffers/lib/concat';
 
 export class RmRecordDecoder {
-  public readonly reader = new StreamingOctetReader();
+  public readonly reader = new StreamingReader();
   protected fragments: Uint8Array[] = [];
 
   public push(uint8: Uint8Array): void {
@@ -9,15 +10,38 @@ export class RmRecordDecoder {
   }
 
   public readRecord(): Uint8Array | undefined {
-    try {
-      const reader = this.reader;
-      const header = reader.u32();
-      const fin = (header & 0b10000000_00000000_00000000_00000000) !== 0;
-      const len = header & 0b01111111_11111111_11111111_11111111;
-
-    } catch (err) {
-      if (err instanceof RangeError) return undefined;
-      throw err;
+    const reader = this.reader;
+    let size = reader.size();
+    if (size < 4) return undefined;
+    const x = reader.x;
+    READ_FRAGMENT: {
+      try {
+        const header = reader.u32();
+        size -= 4;
+        const fin = !!(header & 0b10000000_00000000_00000000_00000000);
+        const len = header & 0b01111111_11111111_11111111_11111111;
+        if (size < len) break READ_FRAGMENT;
+        const currentFragment = reader.buf(len);
+        reader.consume();
+        const fragments = this.fragments;
+        // console.log('header', size, header, fin, len, fragments, currentFragment);
+        if (fin) {
+          if (!fragments.length) return currentFragment;
+          fragments.push(currentFragment);
+          const record = concatList(fragments);
+          this.fragments = [];
+          return record.length ? record : undefined;
+        } else {
+          fragments.push(currentFragment);
+          return undefined;
+        }
+      } catch (err) {
+        reader.x = x;
+        if (err instanceof RangeError) return undefined;
+        else throw err;
+      }
     }
+    reader.x = x;
+    return undefined;
   }
 }

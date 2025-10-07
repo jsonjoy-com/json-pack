675a6562777dff75100c699bd375d9146e2a6a9e
streamich <streamich@gmail.com>
2025-10-07 15:48:55 +0200

    feat: 🎸 update record encoder implementation

--- src/rm/RmRecordEncoder.ts
+++ src/rm/RmRecordEncoder.ts
@@ -15,10 +15,29 @@ export class RmRecordEncoder<W extends IWriter & IWriterGrowable = IWriter & IWr
   }
 
   public writeHdr(fin: 0 | 1, length: number): void {
-    // ...
+    this.writer.u32((fin ? 0b10000000_00000000_00000000_00000000 : 0) + length);
   }
 
   public writeRecord(record: Uint8Array): void {
-    // ..
+    const length = record.length;
+    if (length <= 2147483647) {
+      const writer = this.writer;
+      writer.u32(0b10000000_00000000_00000000_00000000 + length);
+      writer.buf(record, length);
+      return;
+    }
+    let offset = 0;
+    while (offset < length) {
+      const fragmentLength = Math.min(length - offset, 0x7fffffff);
+      const fin = fragmentLength + offset >= length ? 1 : 0;
+      this.writeFragment(record, offset, fragmentLength, fin);
+      offset += fragmentLength;
+    }
+  }
+
+  public writeFragment(record: Uint8Array, offset: number, length: number, fin: 0 | 1): void {
+    this.writeHdr(fin, length);
+    const fragment = record.subarray(offset, offset + length);
+    this.writer.buf(fragment, length);
   }
 }
